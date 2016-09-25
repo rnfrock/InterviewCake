bc348e04a1dd845ff7262bb8bf903d725dc0da30
Frock <rachel.frock@us.sogeti.com>
2016-09-25 15:42:08 -0500

    Initial setup

---
+++ Solutions/9_25_16_AppleStocks_Soln.cs
@@ -0,0 +1,21 @@
+// 9/25/2016 Apple Stocks Interview Question
+
+
+using System;
+
+namespace 9_25_16_AppleStocks_Soln
+{
+
+class Solution{
+    public static string MyFunction(string arg){
+        // write the body of your function here
+        return "running with " + arg;
+    }
+
+    static void Main(string[] args){
+        // run your function through some test cases here
+        // remember: debugging is half the battle!
+        Console.WriteLine(MyFunction("test input"));
+    }
+}
+}
