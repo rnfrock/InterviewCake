2184a1d5dca9906fc4ecb3bc9ff99bfdee9607a0
Frock <rachel.frock@us.sogeti.com>
2016-09-26 21:35:59 -0500

    first stab at a solution

--- Solutions/9_25_16_AppleStocks_Soln.cs
+++ Solutions/9_25_16_AppleStocks_Soln.cs
@@ -6,16 +6,16 @@ using System;
 namespace 9_25_16_AppleStocks_Soln
 {
 
-class Solution{
-    public static string MyFunction(string arg){
-        // write the body of your function here
-        return "running with " + arg;
-    }
+    class Solution{
+        public static string MyFunction(string arg){
+            // write the body of your function here
+            return "running with " + arg;
+        }
 
-    static void Main(string[] args){
-        // run your function through some test cases here
-        // remember: debugging is half the battle!
-        Console.WriteLine(MyFunction("test input"));
+        static void Main(string[] args){
+            // run your function through some test cases here
+            // remember: debugging is half the battle!
+            Console.WriteLine(MyFunction("test input"));
+        }
     }
-}
 }
