4b30b348b349b0779a72c0944fdebbcd8bd114e5
Frock <rachel.frock@us.sogeti.com>
2016-09-26 21:45:18 -0500

    need to deal with negative case

--- Solutions/9_25_16_AppleStocks_Soln.cs
+++ Solutions/9_25_16_AppleStocks_Soln.cs
@@ -3,19 +3,44 @@
 
 using System;
 
-namespace 9_25_16_AppleStocks_Soln
+namespace _9_25_16_AppleStock_Soln
 {
+    class Solution
+    {
 
-    class Solution{
-        public static string MyFunction(string arg){
+        public static string get_max_profit(int[] arg)
+        {
             // write the body of your function here
-            return "running with " + arg;
+            int bestBuy = arg[0];
+            int buyIdx = 0;
+            int bestSell = arg[0];
+            int sellIdx = 0;
+
+            for( int i = 2; i < arg.Length; i++ )
+            {
+                if( arg[i] < bestBuy )
+                {
+                    buyIdx = i;
+                    bestBuy = arg[i];
+                }
+                if( sellIdx < buyIdx || (i > buyIdx && arg[i] > bestSell) )   
+                {
+                    sellIdx = i;
+                    bestSell = arg[i];
+                }
+                // need case if the rest are just a decline...
+                // need best negative case
+            }
+            return "running with " + bestBuy + ", " + bestSell;
         }
 
-        static void Main(string[] args){
+
+        static void Main(string[] args)
+        {
             // run your function through some test cases here
             // remember: debugging is half the battle!
-            Console.WriteLine(MyFunction("test input"));
+            int[] stock_prices_yesterday = { 10, 7, 5, 8, 11, 9 };
+            Console.WriteLine(get_max_profit(stock_prices_yesterday));
         }
     }
 }
