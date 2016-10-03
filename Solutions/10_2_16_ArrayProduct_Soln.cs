a7f28d9e518980aa9e474434519665c2fba7595d
rnfrock <rachel.frock@utexas.edu>
2016-10-03 15:40:09 -0500

    clean code organization

--- Solutions/10_2_16_ArrayProduct_Soln.cs
+++ Solutions/10_2_16_ArrayProduct_Soln.cs
@@ -10,31 +10,30 @@ namespace _10_2_16_ArrayProduct_Soln
         public static int[] getProductsOfAllIntsExceptAtIndex( int[] input )
         {
             int l = input.Length;
-            int[] ans = new int[ l ];
-            
+            int[] ans = new int[ l ];          
             int firstProd = 1;
             int secondProd = 1;
-            int endIdx;
             for( int i = 0; i < l - 1; i++ )
             {
-                endIdx = l - (i + 1);
-
+                int endIdx = l - (i + 1);
                 firstProd *= input[i];
                 secondProd *= input[endIdx];
-                //Console.WriteLine("firstProd = " + firstProd + ", secondProd = " + secondProd + ", i = " + i);
-
-                ans[i+1] = (i < ((l * 0.5)-1) || i == l-2) ? firstProd : firstProd * ans[i+1];
-                ans[endIdx-1] = (i < ((l * 0.5)-1) || i == l-2) ? secondProd : secondProd * ans[endIdx-1];
+                //Console.WriteLine("firstProd = " + firstProd + ", secondProd = " + secondProd + ", i = " + i);       
+                if( i < ((l * 0.5)-1) || i == l-2 ) {
+                    ans[i+1] = firstProd;
+                    ans[endIdx-1] = secondProd;
+                }else {
+                    ans[i+1] *= firstProd;
+                    ans[endIdx-1] *= secondProd;
+                }
             }
-
             return ans;
         }
 
         static void Main(string[] args)
         {
             int[] input = { 1, 7, 3, 4 };
-            int[] ans = getProductsOfAllIntsExceptAtIndex(input);
-            foreach( int x in ans )
+            foreach( int x in getProductsOfAllIntsExceptAtIndex(input) )
             {
                 Console.Write(x + " ");
             }
